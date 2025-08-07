87267fd6a19030537c3975b730cd859079b8b824
Krutant Raut <krutantraut.com@gmail.com>
2025-08-07 20:04:32 +0530

    EDA and Problem statements

--- src/pipeline/exception.py
+++ src/pipeline/exception.py
@@ -27,7 +27,8 @@ if __name__=="__main__":
         raise CustomException(e,sys) """
           
 import sys
-import logging
+from src.pipeline.logger import logging
+
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -46,3 +47,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
